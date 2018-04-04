5d352f7dae71a30b185600fd97c3840b66f69dbd
huxii <huxin37118@gmail.com>
2018-04-04 17:00:47 -0400

    II: control & level 1 tuned.

--- Assets/Scripts/PlayerPivotBehavoir.cs
+++ Assets/Scripts/PlayerPivotBehavoir.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class PlayerPivotBehavoir : MonoBehaviour
 {
-    public float distance = 3f;
+    public Vector2 screenLimit = new Vector2(2f, 1f);
 
     GameObject player;
+    Vector3 targetPos;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
+        targetPos = player.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = player.transform.position - transform.position;
-        if (dir.magnitude > distance)
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(player.transform.position);
+        Vector3 pivotScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (Mathf.Abs(playerScreenPos.x - pivotScreenPos.x) > screenLimit.x
+            || Mathf.Abs(playerScreenPos.y - pivotScreenPos.y) > screenLimit.y)
         {
-            transform.Translate(dir.normalized * (dir.magnitude - distance) * Time.deltaTime * 5f);
+            targetPos = player.transform.position;
         }
-	}
+
+        Vector3 dir = targetPos - transform.position;
+        if (dir.magnitude < 0.01f)
+        {
+            transform.position = targetPos;
+        }
+        else
+        {
+            transform.Translate(dir.normalized * Time.deltaTime * 5f);
+        }
+    }
 }
