6dc5648d3574367fe2765e77d04ea57487832fbd
huxii <huxin37118@gmail.com>
2018-04-26 17:23:26 -0400

    II: Level 1 cutscene added.

---
+++ Assets/Scripts/CutSceneBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneBehavior : MonoBehaviour
+{
+    public GameObject videoManager;
+    VideoControl videoController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        videoController = videoManager.GetComponent<VideoControl>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            videoController.Play(VideoControl.VideoListID.LEVEL1CUTSCENE);
+        }
+    }
+}
---
+++ Assets/Scripts/VideoControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class VideoControl : MonoBehaviour
+{
+    public GameObject targetCamera;
+    public GameObject blackImage;
+    public List<VideoClip> videoList;
+    public enum VideoListID
+    {
+        LEVEL1CUTSCENE,
+    };
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Play(VideoListID id)
+    {
+        var videoPlayer = targetCamera.AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.clip = videoList[(int)id];
+        videoPlayer.aspectRatio = VideoAspectRatio.FitOutside;
+        videoPlayer.Pause();
+
+        blackImage.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        blackImage.SetActive(true);
+        videoPlayer.Play();
+        blackImage.GetComponent<Image>().DOFade(1, 2f).OnComplete(
+            () => 
+            {             
+                blackImage.SetActive(false);              
+            }
+            );      
+    }
+}
