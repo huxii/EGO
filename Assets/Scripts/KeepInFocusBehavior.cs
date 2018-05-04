4998bfa0a0660b4c7f37ac5f127be60ea74f69f4
huxii <huxin37118@gmail.com>
2018-05-04 10:41:31 -0400

    II: dynamic DOF.

---
+++ Assets/Scripts/KeepInFocusBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class KeepInFocusBehavior : MonoBehaviour
+{    
+    // Adjustable aperture - used in animations within Timeline
+    [Range(0.1f, 20f)] public float aperture;
+
+    GameObject targetCam;
+    PostProcessingProfile postProfile;
+
+    void Start()
+    {
+        targetCam = GameObject.FindGameObjectWithTag("ScannerBgCamera");
+        // Load the post processing profile
+        postProfile = targetCam.GetComponent<PostProcessingBehaviour>().profile;
+    }
+
+    void Update()
+    {
+        // Get distance from camera and target
+        float dist = Vector3.Distance(targetCam.transform.position, transform.position);
+
+        // Get reference to the DoF settings
+        var dof = postProfile.depthOfField.settings;
+
+        // Set variables
+        dof.focusDistance = dist;
+        dof.aperture = aperture;
+
+        // Apply settings
+        postProfile.depthOfField.settings = dof;
+    }
+}
