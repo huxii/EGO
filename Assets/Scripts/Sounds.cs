7d4eb17629cc2e86d994d95273aa373ea88959a5
Unknown <kz1065@nyu.edu>
2018-05-06 23:44:14 -0400

    Sound script filled

--- Assets/Scripts/Sounds.cs
+++ Assets/Scripts/Sounds.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 
 [System.Serializable]
 public class SoundSettings
 {
     public SoundEnum id;
-    public Vector3 pos = new Vector3(0f, 0f, 0f);
+    public ClipType type = ClipType.NONE;
+    public Transform pos;
     public float delay = 0f;
+    public float fadeDuration = 3f;
     public bool isLooping = false;
     public bool isFalloff = false;
     public bool isDestroiedAfterFinish = false;
+    public bool isStandOut = false;
+    public float spatialBlend = 0;
 }
 
 
@@ -20,11 +25,31 @@ public class Sounds
 {
     public virtual void Play(SoundSettings settings)
     {
-
+        if (settings.id == SoundEnum.NONE)
+        {
+            return;
+        }
+        int i = settings.isFalloff ? 1 : 0;
+        GameObject eff = NewPooledObject.current.GetSoundEffect();
+        AudioSource source = eff.GetComponent<AudioSource>();
+        SoundEffectController sec = eff.GetComponent<SoundEffectController>();
+        source.clip = audio;
+        source.loop = settings.isLooping;
+        source.volume = 0;
+        source.spread = i * 360;
+        source.spatialBlend = settings.spatialBlend;
+        eff.transform.position = settings.pos.position;
+        sec.c = settings.type;
+        sec.needTurnOff = settings.isDestroiedAfterFinish;
+        if (settings.isStandOut) {
+            //fade other sounds
+        }
+        source.PlayDelayed(settings.delay);
+        source.DOFade(1, settings.fadeDuration);
     }
 
-    AudioClip audio;
-    AudioSource musicPlayer;
+    protected AudioClip audio;
+    protected AudioSource musicPlayer;
 }
 
 public class BGM : Sounds
@@ -33,8 +58,19 @@ public class BGM : Sounds
 
     public override void Play(SoundSettings settings)
     {
+        foreach (GameObject i in NewPooledObject.current.Effects) {
+            if (i.activeInHierarchy && i.GetComponent<SoundEffectController>().c == ClipType.BGM)
+            {
+                i.GetComponent<AudioSource>().DOFade(0, 3f).OnComplete(() => { i.SetActive(false); });
+                //
+                i.GetComponent<SoundEffectController>().c = ClipType.NONE;
+
+            }
+        }
+        base.Play(settings);
 
     }
+
 }
 
 public class Ambience : Sounds
