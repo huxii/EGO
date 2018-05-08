577774e40fd663eb138f3761f2a12e8b441bac0b
Salter <sl6576@nyu.edu>
2018-05-08 14:19:23 -0400

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/CutSceneBehavior.cs
+++ Assets/Scripts/CutSceneBehavior.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class CutSceneBehavior : MonoBehaviour
 {
-    public GameObject videoManager;
-    VideoControl videoController;
 
 	// Use this for initialization
 	void Start ()
     {
-        videoController = videoManager.GetComponent<VideoControl>();
-
     }
 	
 	// Update is called once per frame
@@ -24,7 +20,7 @@ public class CutSceneBehavior : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            videoController.Play(VideoControl.VideoListID.LEVEL1CUTSCENE);
+			GameControl.videoController.Play(VideoControl.VideoListID.LEVEL1CUTSCENE);
         }
     }
 }
