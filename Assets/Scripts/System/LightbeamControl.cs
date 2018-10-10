f794fea28c59d6b698f655bde4d18bf579c1a428
huxii <huxin37118@gmail.com>
2018-10-10 17:41:00 -0400

    Level mechanics recovered.

---
+++ Assets/Scripts/System/LightbeamControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightbeamControl : TransitionEffectControl
+{
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            timer -= Time.deltaTime * speed;
+            foreach (Material mat in mats)
+            {
+                mat.SetFloat("_Tweak", timer);
+            }
+
+            if (timer <= -5f)
+            {
+                Done();
+            }
+        }
+    }
+
+    public override void Play()
+    {
+        base.Play();
+        timer = 4.0f;
+        foreach (Material mat in mats)
+        {
+            mat.SetFloat("_Tweak", timer);
+        }
+    }
+}
